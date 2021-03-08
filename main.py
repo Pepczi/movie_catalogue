353a9b28445e79269084cfe81669685da91cb239
Piotr Pawełczak <pawelczak.piotr@gmail.com>
2021-03-08 07:15:32 +0100

    Final version

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-from flask import Flask, render_template
-import tmdb_client
+from flask import Flask, render_template, request
+from tmdb_client import tmdb
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
-def homepage(url_movie = "https://api.themoviedb.org/3/movie/popular"):
-    movies = tmdb_client.get_json(url_movie)
-    movie_id = tmdb_client.get_movie_id(url_movie)
-    return render_template("homepage.html", movies=movies, movie_id=movie_id)
+def homepage():
+    selected_list = request.args.get('list_type', "popular")
+    movies = tmdb.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list)
 
 @app.route("/movie/<movie_id>")
 def movie_details(movie_id):
-    details = tmdb_client.get_movie_details(movie_id)
-    return render_template("movie_details.html", movie=details)
+    details = tmdb.get_single_movie(movie_id)
+    cast = tmdb.get_single_movie_cast(movie_id)
+    return render_template("movie_details.html", movie=details, cast=cast)
 
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb.get_poster_movie(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,53 +1,59 @@
 import requests
-import pprint
 
-pp = pprint.PrettyPrinter()
+API_TOKEN = ""
 
-def get_json(url_api):
-    #url = "https://api.themoviedb.org/3/movie/popular"
-    url = url_api
-    headers = {'Authorization' : "Bearer "}
-    respone = requests.get(url,headers=headers)
-    return respone.json()
+class Movie_data():
+    def __init__(self):
+        self.headers = {'Authorization' : f"Bearer {API_TOKEN}"}
 
-def configuration_json():
-    url = "https://api.themoviedb.org/3/configuration"
-    headers = {'Authorization' : "Bearer "}
-    configuration = requests.get(url,headers=headers)
-    return configuration.json()
+    def get_configuration(self):
+        config = "https://api.themoviedb.org/3/configuration"
+        configuration = requests.get(config, headers=self.headers)
+        return configuration.json()
 
-def get_movie_id(url_api):
-    data = get_json(url_api)
-    id = ['results']['id']
+    def get_movie_info(self, url_api):
+        respone = requests.get(url_api, headers=self.headers)
+        return respone['results'].json()
+    
+    def get_id(self, url_api):
+        data = self.get_movie_info()
+        return data['results']['id']
+
+    def get_popular_movies(self, list_type='popular'):
+        endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+        response = requests.get(endpoint, headers=self.headers)
+        return response.json()
+
+    def get_poster_movie(self, poster_path, size='w342'):
+        base_url = "https://image.tmdb.org/t/p/"
+        return f"{base_url}{size}{poster_path}"
+
+    def get_movies(self, how_many, list_type='popular'):
+        data = self.get_popular_movies(list_type)
+        return data["results"][:how_many]
+
+    def get_single_movie(self, movie_id):
+        endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+        response = requests.get(endpoint, headers=self.headers)
+        return response.json()
+
+    def get_single_movie_cast(self, movie_id):
+        endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+        response = requests.get(endpoint, headers=self.headers)
+        return response.json()
+
+    def get_movies_list(self, list_type):
+        endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+        response = requests.get(endpoint, headers=self.headers)
+        response.raise_for_status()
+        return response.json()
+
+    def get_movies_list(self):
+        endpoint = "https://api.themoviedb.org/3/genre/movie/list"
+        response = requests.get(endpoint, headers=self.headers)
+        return response.json()
 
 
-def get_movie_details(id):
-    url_for_id =  f"{'https://api.themoviedb.org/3/movie/'}{id}"
-    data = get_json(url_for_id)
-    return data
-
-
-def poster_url(url_api,size='w342'):
-    configuartion = configuration_json()
-    movie_info = get_json(url_api)
-    base_url = f"{configuartion['images']['base_url']}{size}"
-    posters_url_list = []
-    for info in movie_info['results']:
-        posters_url_list.append(f"{base_url}{info['poster_path']}")
-    return posters_url_list
-
-def title_posters_list(url_api, size='w342'):
-    movie_info = get_json(url_api)
-    url_poster = poster_url(url_api, size)
-    title_poster = []
-    for info in movie_info['results']:
-        title_poster.append(f"{info['title']}")
-    posters_url_list = []
-    for i, k in zip(title_poster, url_poster):
-        posters_url_list.append({i : k})
-    return posters_url_list
-
-
-
+tmdb = Movie_data()
 
 
